a62f70e1d03234a7c612cf06a0b99dafbbbcff31
NelsonBlaCha <nelson.blacha@educa.jcyl.es>
2023-01-11 19:17:15 +0100

    inicio_Routes_Laravel

---
+++ Tema5Laravel/pruebaLaravel2/resources/views/admin/mensajeAdmin.blade.php
@@ -0,0 +1,14 @@
+<h1>Administrador: {{ $texto }}</h1>
+<br />
+<a href="{{ route('inicio') }}">
+    <h2>Ir a Inicio</h2>
+</a>
+<a href="{{ route('articulos') }}">
+    <h2>Ir a Articulos</h2>
+</a>
+<a href="{{ route('clientes') }}">
+    <h2>Ir a Clientes</h2>
+</a>
+<a href="{{ route('facturas') }}">
+    <h2>Ir a Facturas</h2>
+</a>
---
+++ Tema5Laravel/pruebaLaravel2/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminControlador;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HolaControlador;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HolaControlador::class, 'raiz'])->name("inicio");
+
+Route::match(['get', 'post'], '/saludo/{nombre?}/{apellido?}', [HolaControlador::class, 'saludo'])->name("saludo");
+
+// Route::get('/suma/{numero1?}/{numero2?}/{numero3?}', [HolaControlador::class, 'suma'])->name("suma");
+Route::match(['get', 'post'], '/suma/{numero1?}/{numero2?}/{numero3?}', [HolaControlador::class, 'suma'])->name("suma");
