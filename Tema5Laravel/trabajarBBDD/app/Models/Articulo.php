e30492dbe7498c1f118a55498adab29b0f2f6e26
NelsonBlaCha <nelson.blacha@educa.jcyl.es>
2023-01-18 17:51:09 +0100

    gestionDeBBDD

---
+++ Tema5Laravel/trabajarBBDD/app/Models/Articulo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    use HasFactory;
+    protected $table = 'articulos';
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'color',
+        'precio',
+        'envio',
+        'stock',
+        'observaciones'
+    ];
+    protected $hidden = [];
+}
--- Tema5Laravel/trabajarBBDD/database/seeders/DatabaseSeeder.php
+++ Tema5Laravel/trabajarBBDD/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Articulo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Articulo::factory(1000)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
