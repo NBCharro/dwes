9c8b816ffd0e3d1e3ce31d48324b5481d2fd944f
NelsonBlaCha <nelson.blacha@educa.jcyl.es>
2023-01-30 16:39:51 +0100

    autenficacion

--- Tema5Laravel/ejercicio12/resources/views/layouts/main-layout.blade.php
+++ Tema5Laravel/ejercicio12/resources/views/layouts/main-layout.blade.php
@@ -6,10 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js">
     </script>
+
     <title>{{ $nombre }}</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -23,9 +25,10 @@
         @include('components.footer')
     </div>
 </body>
+
 <script>
     $(document).ready(function() {
-        $('#tablaDiscos').DataTable();
+        $('#miTabla').DataTable();
     });
 </script>
 
--- Tema5Laravel/trabajarBBDD/app/Http/Controllers/MainController.php
+++ Tema5Laravel/trabajarBBDD/app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Articulo;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,5 +10,21 @@ class MainController extends Controller
     public function inicio()
     {
         return view('main-page');
+    }
+    public function querySQL(){
+        // $articulos = Articulo::all()->orderBy('precio','desc')->->limit(5)->get();
+        $articulos = Articulo::where('nombre','like','a%')->orderBy('nombre','asc')->get();
+        $articulos = Articulo::where('nombre','like','a%s.')->orderBy('nombre','asc')->get();
+        $articulos = Articulo::where('nombre', 'like', 'a%')->orWhere('nombre', 'like', 'a%')->orderBy('precio', 'desc')->get();
+        $articulos = Articulo::where('nombre', 'like', 's%')->where('precio', '>', '100')->where('precio', '<', '200')->get();
+        $articulos = Articulo::where('stock', '<', '100')->count()->get();
+        $articulos = Articulo::where('precio', '>', '500')->avg('stock')->get();
+        $articuloMin = Articulo::min('precio');
+        $articuloMax = Articulo::max('precio');
+        $articulos = Articulo::where('precio', $articuloMin)->orWhere('precio', $articuloMax)->get();
+        dump($articuloMin);
+        dump($articuloMin);
+        dump($articuloMax);
+        dump($articulos);
     }
 }
